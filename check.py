a548b10b6e988ea5a81552d2284f35295f380c5f
Giridhara Chary <giridhar22042006@gmail.com>
2026-05-29 19:21:48 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,1 +1,8 @@
-print("RVRJC Results Monitor Running")
+import requests
+
+url = "https://rvrjcce.ac.in/examcell/results/resultsN.php?page_no=1"
+
+response = requests.get(url)
+
+print("Status Code:", response.status_code)
+print("Page Length:", len(response.text))
