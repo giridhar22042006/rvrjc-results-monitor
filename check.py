6f3a21c6fcb626a4c7ca9252df47c87230918c63
Giridhara Chary <giridhar22042006@gmail.com>
2026-05-29 19:31:27 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,8 +1,30 @@
 import requests
+import re
 
-url = "https://rvrjcce.ac.in/examcell/results/resultsN.php?page_no=1"
+URL = "https://rvrjcce.ac.in/examcell/results/resultsN.php?page_no=1"
 
-response = requests.get(url)
+response = requests.get(URL)
+html = response.text
 
-print("Status Code:", response.status_code)
-print("Page Length:", len(response.text))
+pattern = r'B\.Tech\..*?March-2026-Revaluation'
+match = re.search(pattern, html)
+
+if not match:
+    print("Could not find latest result")
+    exit()
+
+current_result = match.group(0)
+
+with open("last_result.txt", "r") as f:
+    stored_result = f.read().strip()
+
+print("Stored Result:")
+print(stored_result)
+
+print("\nCurrent Result:")
+print(current_result)
+
+if current_result != stored_result:
+    print("\nNEW RESULT DETECTED!")
+else:
+    print("\nNo new result.")
