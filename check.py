3497c1ab97a7f762c9139eebd91dd818a1da5ad9
Giridhara Chary <giridhar22042006@gmail.com>
2026-05-29 19:52:45 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
+import os
+from email.mime.text import MIMEText
 
 URL = "https://rvrjcce.ac.in/examcell/results/resultsN.php?page_no=1"
 
@@ -27,7 +30,36 @@ with open("last_result.txt", "r", encoding="utf-8") as f:
 print("Stored:", stored_result)
 print("Latest:", latest_result)
 
-if stored_result == latest_result:
-    print("No new result.")
-else:
+if stored_result != latest_result:
+
     print("NEW RESULT DETECTED!")
+
+    sender = os.environ["EMAIL_USER"]
+    password = os.environ["EMAIL_PASS"]
+    receiver = os.environ["EMAIL_TO"]
+
+    body = f"""
+New RVRJC Result Detected
+
+{latest_result}
+
+Website:
+https://rvrjcce.ac.in/examcell/results/resultsN.php?page_no=1
+"""
+
+    msg = MIMEText(body)
+    msg["Subject"] = "RVRJC Results Update"
+    msg["From"] = sender
+    msg["To"] = receiver
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(sender, password)
+    server.sendmail(sender, receiver, msg.as_string())
+    server.quit()
+
+    with open("last_result.txt", "w", encoding="utf-8") as f:
+        f.write(latest_result)
+
+else:
+    print("No new result.")
